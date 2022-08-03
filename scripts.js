64779bfa539c6dc04726e2720f7eab47f866fc47
mason madaras <m.madaras@masons-MacBook-Air.local>
2022-08-03 16:35:20 -0400

    pasta site bocaweb

--- scripts.js
+++ scripts.js
@@ -1,34 +1,19 @@
-console.log(' this is working ')
+const menuElement = document.querySelector('#menu')
 
-const car = {
-    brand: 'Tesla',
-    notAwesome: true,
-    overRated: true,
-    saveGas: true,
-    favorites: ['tech', 'battery', 'interior']
-}
+fetch('https://codice-boca.web.app/menu')
+.then(res => res.json())
+.then(menuItems => {
+    let html = ''
 
+    menuItems.forEach((recipe) => {
+      
+     let htmlSegment = `<div class="menu-item">
+     <h3>${recipe.title}</h3>
+     <p>${recipe.description}</p>
+ </div>`
 
-// grab my p1 id
-// change the text inside via JS
-const p1 = document.getElementById('p1')
-
-
-
-p1.innerHTML = 'Hey Class!'
-
-p1.innerHTML = '<h2> second heading </h2>'
-
-const changeColor = () => {
-    p1.style.color = 'blue'
-}
-
-const myFunction = () =>{
-    alert('TEST')
-}
-
-const getSomeData = () => {
-  fetch('https://codice-boca.web.app/menu')
-  .then(response => response.json())
-  .then(cleanData => console.log(cleanData))
-}
+     html += htmlSegment
+    })
+ menuElement.innerHTML = html
+})
+.catch(err => console.error(err))
